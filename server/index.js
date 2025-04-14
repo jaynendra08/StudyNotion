4b5424f667066a4e43c094bed26478edcf19d4c0
jaynendra08 <jaynendrasingh08@gmail.com>
2025-04-15 02:44:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "*",
+		origin: "https://study-notion-frontend-gray-sigma.vercel.app",
 		credentials: true,
 	})
 );
